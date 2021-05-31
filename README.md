a6a44940791f15378f7f1fbb3529cec127b3c399
Stephy-Cheung <stephysmile.sc@gmail.com>
2021-06-01 00:49:06 +0800

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
+<img src="img/Example.png" width="600">
+
 # Exercise-Streamlit_Anime_dataset
+
+## Objective
+This is an exercise to develop an anime recommendation app on Streamlit. Recommendation will be made base on user favourite anime, user_id and similarity distance measure method. <br>
+
+'Recommend_app(streamlit)' is code for the Streamlit ap. <br>
+'Preprocessing' is code for the pre-processing on dataset. <br>
+
+Data is from kaggle dataset: https://www.kaggle.com/CooperUnion/anime-recommendations-database
+
+## Pre-processing on dataset 
+There is two data file in this dataset. <br>
+
+'Anime.csv' contain the information of the anime, including the anime_id, name of anime, genre, type, rating and number of episodes. <br>
+
+'Rating.csv' records the anime that watched by the user and the rating. Rating is marked as '-1' if user did not rate the anime. <br>
+
+Three data file prepared for the streamlit app. 
+
+### anime.csv
+Contain basic details of the anime. Index by 'anime_id', followed by the name, genre and rating. 
+<img src="" width="600">
+
+### anime_genre.csv
+Datafile generated from Anime.csv for similarity distance measure. Genre and typ columns are One-Hot Encoded and rating is scaled to 0-1, index by 'anime_id'.
+<img src="" width="600">
+
+### rating.csv
+No preprocessing on this datafile.
+<img src="" width="600">
+
+
+## Application
+User are able to input the below for recommendation. 
+- Distance method
+- Previous favourite anime
+- Number of recommendation wanted
+- user_id 
+
+## Result:
+Recommendations that haven't been watched by user will show on the first part and followed by recommendation for the re-watch as the second part. The print out order is according to the similarity distance of user favourite anime.
+<img src="image/Example.png" width="400">
+
+## Next Step:
+1. User Input: 
+    Instead of input guest favourite anime, the input parameters can be changed to the preferred genre for more flexibility on the selection on anime. 
+
+2. Rating:
+    A content-based recommendation is used in this application as the genre and type of anime is completed, while there is large among of missing values in the rating.csv. A lot of the users didn't rate the anime that they have watched. A collaborative filtering recommendation can be develop if a more completed rating data obtained in the future.
+
---
+++ Recommend_app(streamlit).py
@@ -0,0 +1,71 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.io.parsers import read_csv
+import seaborn as sns
+
+import streamlit as st
+from scipy.spatial.distance import euclidean, cityblock, cosine
+
+def get_data():
+    anime = pd.read_csv('anime_data/anime.csv')
+    anime=anime.set_index('anime_id')
+    anime_genre = pd.read_csv('anime_data/anime_preprocessed.csv')
+    anime_genre=anime_genre.set_index('anime_id')
+    rating = pd.read_csv('anime_data/rating.csv')
+    return anime, anime_genre, rating
+
+
+def anime_recommender(distance, anime_id, N):
+    # dataframe to store distance between animes 
+    anime_distance = pd.DataFrame (data = anime_genre.index)
+
+    # drop row of the input anime_id
+    anime_distance = anime_distance[anime_genre.index != anime_id]
+
+    # Calculate distance and store in distance columnm
+    if distance == 'euclidean':
+        anime_distance['distance'] = anime_distance['anime_id'].apply(lambda x: euclidean(anime_genre.loc[x],anime_genre.loc[anime_id]))
+    elif distance == 'manhattan':
+        anime_distance['distance'] = anime_distance['anime_id'].apply(lambda x: cityblock(anime_genre.loc[x],anime_genre.loc[anime_id]))
+    elif distance == 'cosine':
+        anime_distance['distance'] = anime_distance['anime_id'].apply(lambda x: cosine(anime_genre.loc[x],anime_genre.loc[anime_id]))
+    anime_distance.sort_values(by='distance', inplace= True)
+    
+    return (anime_distance.head(N))
+
+def result(id):
+    st.text('Title: ' + anime.loc[r, 'name']+ '      Rating: '+ str(anime.loc[r, 'rating']))
+    st.text('Genre: '+ anime.loc[r, 'genre'])
+    st.text(' ')
+
+
+anime, anime_genre, rating = get_data()
+
+st.title ('Anime Recommendation')
+st.text ('This application will make anime recommendation')
+st.text (' base on user previous favourite anime.')
+
+# Sidebar - Obtain user input 
+distance = st.sidebar.selectbox ('Distance method: ', ['euclidean','manhattan','cosine'])
+anime_name = st.sidebar.selectbox('Previous Favourite : ',anime['name'].tolist())
+num_re = st.sidebar.slider('Number of recommendation : ',min_value=1, max_value =10, step =1)
+user_id = st.sidebar.number_input('Enter your user id: (key 0 if you are not a member)')
+
+#Recommendation
+st.title('Recommendation')
+recommendation = anime_recommender(distance, anime[anime['name']==anime_name].index[0], num_re)
+
+if user_id not in rating['user_id']: 
+    st.subheader('You may like the below animes...')
+    for r in recommendation['anime_id']:
+        result(r)
+else: 
+    watched = rating[rating['user_id']==user_id]
+    st.subheader('You may like the below animes...')
+    for r in recommendation['anime_id']:
+        if r not in watched['anime_id']:
+            result(r)
+    st.subheader('You may also re-watched the below animes...')
+    for r in recommendation['anime_id']:
+        if r in watched['anime_id']:
+            result(r)
